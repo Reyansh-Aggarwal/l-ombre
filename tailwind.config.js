6e2ff7503f6dc02f5838a1b56af54eb0fa6c0317
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2025-04-29 00:28:21 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,20 +12,26 @@ module.exports = {
         'clash-display-light': ['clash-display-light', 'arial'],
       },
       colors: {
-        'dark-red': '#9d1921',
+        'dark-red': '#b41a33',
         'light-red': '#a5132a',
+        'imp-gold': '#ff931c'
       },
       backgroundImage: {
         'fre-art': "url('https://reyansh-aggarwal.github.io/l-ombre/assets/french-art.PNG')"
       },
       animation: {
         gradient: 'gradient 3s ease infinite',
+        'slow-spin': 'slow-spin 30s linear infinite',
       },
       keyframes: {
         gradient: {
           '0%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
           '100%': { backgroundPosition: '0% 50%' },
+        },
+        'slow-spin': {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
         },
       },
     }
