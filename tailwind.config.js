898a7f55d255801eee3f4d80c3c45fc193da8f17
Reyansh-Aggarwal <reyanshmsm@gmail.com>
2025-04-28 18:50:15 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  theme: {
+    backgroundImage: {
+        'black-grid': "url('https://reyansh-aggarwal.github.io/chisel/assets/blackGrid.png')",
+        'fre-art': "url('')"
+
+      },
+  },
+  plugins: [],
+}
+
